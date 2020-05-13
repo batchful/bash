2a9949cd2a408ca988f3e44fc887083a99b3b4f3
SFR <57151943+SFR-git@users.noreply.github.com>
2020-05-13 23:49:15 +0300

    Create batchful.sh
    Started work on a bash program

---
+++ batchful.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Options:
+
+if [[ $1 = "--name" ]] || [[ $1 = "-n" ]]; then # by file name
+  echo "placeholder name"
+
+if [[ $1 = "--type" ]] || [[ $1 = "-t" ]]; then # by file extension
+  echo "placeholder type"
+
+if [[ $1 = "--phrase" ]] || [ $1 = "-p" ]; then # by phrase
+  echo "placeholder phrase"
+
+if [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then # help
+  echo "placeholder help"
+
+fi
+
+
+## WIP

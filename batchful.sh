d57048cffe094f1e8d8934fee77eac7df9a184d8
SFR <57151943+SFR-git@users.noreply.github.com>
2020-05-14 09:53:24 +0300

    Update batchful.sh

--- batchful.sh
+++ batchful.sh
@@ -5,16 +5,24 @@
 if [[ $1 = "--name" ]] || [[ $1 = "-n" ]]; then # by file name
   echo "placeholder name"
 
-if [[ $1 = "--type" ]] || [[ $1 = "-t" ]]; then # by file extension
+else if [[ $1 = "--type" ]] || [[ $1 = "-t" ]]; then # by file extension
   echo "placeholder type"
 
-if [[ $1 = "--phrase" ]] || [ $1 = "-p" ]; then # by phrase
-  echo "placeholder phrase"
+# else if [[ $1 = "--phrase" ]] || [ $1 = "-p" ]; then # by phrase
+#   echo "placeholder phrase"
 
-if [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then # help
+else if [[ $1 = "--github" ]] || [[ $1 = "-g" ]]; then # link to GitHub
+  echo "https://github.com/3174N/batchful"
+
+else if [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then # help
   echo "placeholder help"
 
-fi
+else
+  echo "placeholder error"
 
+fi
+fi
+fi
+fi
 
 ## WIP

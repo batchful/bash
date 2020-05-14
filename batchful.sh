8e5694f3451484c7a2f4cac7b6887323f6a4ca53
SFR <57151943+SFR-git@users.noreply.github.com>
2020-05-14 13:55:53 +0300

    Update batchful.sh
    Added enter option for [--github, -g],
    Added directory validity check

--- batchful.sh
+++ batchful.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-# Options:
+#   _             _         _       __         _
+#  | |__    __ _ | |_  ___ | |__   / _| _   _ | |
+#  | '_ \  / _` || __|/ __|| '_ \ | |_ | | | || |
+#  | |_) || (_| || |_| (__ | | | ||  _|| |_| || |
+#  |_.__/  \__,_| \__|\___||_| |_||_|   \__,_||_|
 
 if [[ $1 = "--name" ]] || [[ $1 = "-n" ]]; then # by file name
-  if [[ $2 = "" ]]; then # Missing directory operand
-    echo batchful: missing directory operand
-    echo Try \'./batchful.sh --help\' for more information.
-  else echo \'$2\' will be sorted by file name.
+if [ -d "$2" ]; then # directory operand validity check
+  echo \'$2\' will be sorted by file name. # need to add actual code
+else
+  echo batchful: invalid directory operand \'$2\'
+  echo Try \'./batchful.sh --help\' for more information.
   fi
 
 elif [[ $1 = "--type" ]] || [[ $1 = "-t" ]]; then # by file extension
-  if [[ $2 = "" ]]; then # Missing directory operand
-    echo batchful: missing directory operand
+  if [ -d "$2" ]; then # directory operand validity check
+    echo \'$2\' will be sorted by file extension. # need to add actual code
+  else
+    echo batchful: invalid directory operand \'$2\'
     echo Try \'./batchful.sh --help\' for more information.
-  else echo \'$2\' will be sorted by file extension.
   fi
 
-# elif [[ $1 = "--phrase" ]] || [ $1 = "-p" ]; then # by phrase
-#   echo "placeholder phrase"
+# phrase
 
 elif [[ $1 = "--github" ]] || [[ $1 = "-g" ]]; then # link to GitHub
   echo batchful: a simple directory organizer
   echo Made by 3174N with help from SFR-git
   read -r -p "Open GitHub repo? [Y/n] " response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then # need to add Enter keypress
+    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]] || [[ $response == "" ]]; then
       xdg-open https://github.com/3174N/batchful/tree/bash
     elif [[ $response =~ ^([nN][oO]|[nN])$ ]]; then
       exit
     else
       echo
-      echo Unrecognised option \'$response\' -- Aborting...
+      echo Invalid response \'$response\' -- Aborting...
     fi
 
 elif [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then # help
-  echo batchful v0.1.0
+  echo batchful v0.2.0-alpha
   echo Usage: ./batchful.sh [OPTION] [DIRECTORY]
   echo
   echo Options:

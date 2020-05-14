c50f12d590fa8dbdbc0c4670e11af82ce11ebe17
SFR <57151943+SFR-git@users.noreply.github.com>
2020-05-14 10:51:10 +0300

    Update batchful.sh
    Finished [-g, --github]

--- batchful.sh
+++ batchful.sh
@@ -5,24 +5,26 @@
 if [[ $1 = "--name" ]] || [[ $1 = "-n" ]]; then # by file name
   echo "placeholder name"
 
-else if [[ $1 = "--type" ]] || [[ $1 = "-t" ]]; then # by file extension
+elif [[ $1 = "--type" ]] || [[ $1 = "-t" ]]; then # by file extension
   echo "placeholder type"
 
-# else if [[ $1 = "--phrase" ]] || [ $1 = "-p" ]; then # by phrase
+# elif [[ $1 = "--phrase" ]] || [ $1 = "-p" ]; then # by phrase
 #   echo "placeholder phrase"
 
-else if [[ $1 = "--github" ]] || [[ $1 = "-g" ]]; then # link to GitHub
-  echo "https://github.com/3174N/batchful"
+elif [[ $1 = "--github" ]] || [[ $1 = "-g" ]]; then # link to GitHub
+  echo "batchful: a simple directory organizer"
+  echo "Made by 3174N with help from SFR-git"
+  read -r -p "Open GitHub repo? [Y/n] " response
+    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then # need to add Enter keypress
+      xdg-open https://github.com/3174N/batchful/tree/bash
+    elif [[ $response =~ ^([nN][oO]|[nN])$ ]]; then
+      exit
+    fi
 
-else if [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then # help
+elif [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then # help
   echo "placeholder help"
 
 else
   echo "placeholder error"
 
 fi
-fi
-fi
-fi
-
-## WIP
